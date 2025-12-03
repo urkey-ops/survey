41083bf442901ef70c43bc2192cd14e16d5214c8
urkey-ops <keyurdudakiya@gmail.com>
2025-12-03 15:33:05 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 // FILE: tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-  // UPDATED: Definitive content configuration array with necessary exclusions
+module.exports = {
   content: [
-    './**/*.html', 
-    './**/*.js', 
-    '!./node_modules/**', // EXCLUSION: Fixes 10-second build time
-    '!./api/**'          // EXCLUSION: Excludes JS in the API folder
+    './*.html', 
+    './*.js',
+    '!./node_modules/**',
+    '!./api/**'
   ],
   theme: {
     extend: {},

ad9f1685a2d7f5eb2056b8a85e93d6439273cab8
urkey-ops <keyurdudakiya@gmail.com>
2025-12-03 18:26:59 +0000

    feat: Add new tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// FILE: tailwind.config.js (MUST BE SAVED WITH THIS CONTENT)
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", 
+    "./*.js" // Scans your JavaScript files 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

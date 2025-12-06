658f935cb6510a0796a5a1ecc27a41ffe514b5c3
urkey-ops <keyurdudakiya@gmail.com>
2025-12-06 14:08:34 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
+// FILE: tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './*.html', 
-    './*.js',
-    '!./node_modules/**',
-    '!./api/**'
-  ],
-  safelist: [
-    'shake'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    './*.html', 
+    './*.js',
+    '!./node_modules/**',
+    '!./api/**'
+  ],
+  safelist: [
+  
+    'animate-pulse',
+    'active-press'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
